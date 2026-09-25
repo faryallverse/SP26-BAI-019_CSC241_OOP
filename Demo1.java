c6bef98924fc1dc49fac183e8e595544a45d922a
Faryal Sarfraz <faryalksarfraz@gmail.com>
2026-09-25 16:30:34 +0500

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,22 @@
+public class Demo1{
+    public static void main(String[] args){
+
+    }
+}
+
+class Overloading{
+   void sum1(int x, int y){
+      int sum = x + y;
+      System.out.println(sum);
+   }
+
+   void sum2(float a, double b){
+      double sum = a + b;
+      System.out.println(sum);
+   }
+
+   void sum3(float m, float n){
+      double sum = m + n;
+      System.out.println(sum);
+   }
+      
